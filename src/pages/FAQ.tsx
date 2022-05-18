f7fc2300b00cd0be443fdf6df581d100a5a845ed
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-18 16:38:59 -0500

    Added More GrayBox Elements

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -4,14 +4,15 @@ import '../styles/App.css';
 function FAQ() {
   return (
     <div className='gray-background'>
-      <p>FAQ</p>
+      <p className='title'>FAQ</p>
+      <p>Frequently Asked Questions</p>
 
       <div className='dual-column'>
         <div>
           <p>QUESTION</p>
           <p>ANSWER</p>
         </div>
-        
+
         <div>
           <p>QUESTION</p>
           <p>ANSWER</p>

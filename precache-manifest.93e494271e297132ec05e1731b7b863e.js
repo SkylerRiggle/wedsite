6207353f6c8f56e4719ffb08dd82bc18e5c921a9
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-18 13:39:47 -0500

    Updates

---
+++ precache-manifest.93e494271e297132ec05e1731b7b863e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "829956844b52f51473bd",
+    "url": "/wedsite/static/css/main.8c51f68a.chunk.css"
+  },
+  {
+    "revision": "829956844b52f51473bd",
+    "url": "/wedsite/static/js/main.82995684.chunk.js"
+  },
+  {
+    "revision": "5f382d9066b7917e9d3b",
+    "url": "/wedsite/static/js/1.5f382d90.chunk.js"
+  },
+  {
+    "revision": "3adc5384745034c0d7df",
+    "url": "/wedsite/static/js/runtime~main.3adc5384.js"
+  },
+  {
+    "revision": "3c460b6a47857b65de7a2965d621cb56",
+    "url": "/wedsite/static/media/dog.3c460b6a.jpg"
+  },
+  {
+    "revision": "fd4b39a858f485a4360a538af21c881c",
+    "url": "/wedsite/index.html"
+  }
+];

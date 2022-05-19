a6ec4dca256c472ebce6c0e571ea36439dceac06
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-19 17:35:51 -0500

    Worked on Venue Section

--- src/pages/Venue.tsx
+++ src/pages/Venue.tsx
@@ -4,7 +4,26 @@ import '../styles/App.css';
 function Venue() {
   return (
     <div className='red-background'>
-      <p>Venue</p>
+      <p className='title'>Venue</p>
+      <div className='dual-column'>
+        <div style={{width:'50%'}}>
+          <p>The Wedding will be held at</p>
+          <p>Wintersmith Lodge in Ada OK</p>
+          <br />
+
+          <p>Wintersmith Lodge,</p>
+          <p>1621 Wintersmith Dr,</p>
+          <p>Ada, OK 74820</p>
+          <br />
+
+          <p></p>
+        </div>
+
+        <div style={{width:'50%'}}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.5840549402824!2d-96.65605428443897!3d34.76606928722143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b369526f215d0b%3A0x9e2ae27c53079950!2sWintersmith%20Lodge!5e0!3m2!1sen!2sus!4v1652998666340!5m2!1sen!2sus" 
+          width="85%" style={{border:'2px solid goldenrod', aspectRatio:'1/1'}} loading="lazy"></iframe>
+        </div>
+      </div>
     </div>
   );
 }

c213f37d0bd1fd271d2975c1f702716e2afb4eb1
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-19 12:05:25 -0500

    Updates

---
+++ precache-manifest.2971d736536986c44a93241d31bfa22a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f97d39df7f0be9d1cf",
+    "url": "/wedsite/static/css/main.cd60f0de.chunk.css"
+  },
+  {
+    "revision": "46f97d39df7f0be9d1cf",
+    "url": "/wedsite/static/js/main.46f97d39.chunk.js"
+  },
+  {
+    "revision": "cf4006990074e7ea3fc9",
+    "url": "/wedsite/static/js/1.cf400699.chunk.js"
+  },
+  {
+    "revision": "3adc5384745034c0d7df",
+    "url": "/wedsite/static/js/runtime~main.3adc5384.js"
+  },
+  {
+    "revision": "2e936c9ab99da46de7514ed122d20d89",
+    "url": "/wedsite/static/media/twohands.2e936c9a.jpg"
+  },
+  {
+    "revision": "c12c507edd2b6e42aff3661308bc3763",
+    "url": "/wedsite/static/media/signature.c12c507e.png"
+  },
+  {
+    "revision": "11d02e892ce25c6d2a10222e1b8fa88d",
+    "url": "/wedsite/static/media/sunflower.11d02e89.png"
+  },
+  {
+    "revision": "4d4745d306f9b2a4b50e8297138cb6a5",
+    "url": "/wedsite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wedsite/precache-manifest.1ea101617a5aec10cd659fd5960c7aba.js"
+  "/wedsite/precache-manifest.2971d736536986c44a93241d31bfa22a.js"
 );
 
 workbox.clientsClaim();

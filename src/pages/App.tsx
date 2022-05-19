41cfcb6e9863b8226ad488646f462a08a3e9362d
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-19 12:04:30 -0500

    Finalized Header Band

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../styles/App.css';
 
+// Image imports
+import Signatures from '../images/signature.png'
+import Sunflower from '../images/sunflower.png'
+
 // Page element imports
 import Header from './Header'
 import RSVP from './RSVP'
@@ -13,6 +17,12 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div style={{backgroundColor:'#5F8575', height:'2.5vw', backgroundImage:'url('+Sunflower+')', 
+      backgroundSize:'auto 2vw', backgroundRepeat:'repeat-x', backgroundPosition:'center'}}>
+        <img src={Signatures} alt="signatures" 
+        style={{width:'10%', backgroundColor:'white', boxShadow:'0 0 5px 0 black',
+        borderRadius:'50%', border:'5px solid goldenrod', position:'relative', bottom:'4vw'}}/>
+      </div>
       <RSVP></RSVP>
       <Schedule></Schedule>
       <Venue></Venue>

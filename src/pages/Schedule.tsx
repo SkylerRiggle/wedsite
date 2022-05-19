bcdf8feb076634b5c4dd311c53a4ac6fdc5ec1f2
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-19 15:28:09 -0500

    Added Schedule Section

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import '../styles/App.css';
+import Flower from '../images/bwflower.png'
+import { transform } from 'typescript';
 
 function Schedule() {
   return (
-    <div className='bw-background'>
-      <p>Schedule</p>
+    <div className='bw-background' style={{marginTop:'5vw', position:'relative', zIndex:'-2'}}>
+      <div style={{width:'75%', textAlign:'left', margin:'2%', marginTop:'5vw'}}>
+        <p className='title' style={{fontSize:'7vw'}}>Schedule</p>
+
+        <div style={{fontWeight:'bold', fontSize:'3vw'}}>
+          <p>&nbsp;&nbsp;
+            ⊰ 10:30 AM . . . . . . . . . . . . . . Rehearsal</p>
+          <p style={{backgroundColor:'#CFCFCF'}}>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 1:30 PM . . . . . . . . . . . . . . Arrival</p>
+          <p>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 2:00 PM . . . . . . . . . . . . . . Ceremony</p>
+          <p style={{backgroundColor:'#CFCFCF'}}>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 2:30 PM . . . . . . . . . . . . . . Group Pictures</p>
+          <p>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 3:30 PM . . . . . . . . . . . . . . Speeches & Slideshow</p>
+          <p style={{backgroundColor:'#CFCFCF'}}>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 4:00 PM . . . . . . . . . . . . . . Cake Cutting</p>
+          <p>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 4:20 PM . . . . . . . . . . . . . . First Dance</p>
+          <p style={{backgroundColor:'#CFCFCF'}}>&nbsp;&nbsp;
+            ⊰&nbsp;&nbsp; 4:30 PM . . . . . . . . . . . . . . Cornhole Tournament</p>
+        </div>
+      </div>
+
+      <div style={{width:'25%', height:'0vw'}}>
+        <img src={Flower} alt="flower" style={{width:'40vw', transform:'translate(0, -5vw)'}}/>
+        <img src={Flower} alt="flower" style={{width:'35vw', transform:'rotate(90deg) translate(-8vw, 0)'}}/>
+      </div>
     </div>
   );
 }

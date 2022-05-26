25c13ccb26b411cc837750cd5232945c666f9546
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-05-26 09:39:14 -0500

    Venue Section

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -12,13 +12,13 @@ function FAQ() {
       <div className='dual-column' style={{paddingTop:'20px'}}>
         <div className='q-card'>
           <p className='dark'>What foods will be served?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>Since the event will be held at 2PM, this should give attendees a chance to eat lunch before the ceremony. Afterward, we will have drinks, a large charcuterie board (finger foods), and cakes.</p>
         </div>
 
         <div className='q-card'>
           <p className='dark'>Should I match the wedding colors/What are the wedding colors?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>You do not have to match our colors! If you'd like to wear matching colors, our colors are reds, yellows, pinks, and greens. </p>
         </div>
       </div>
@@ -26,13 +26,13 @@ function FAQ() {
       <div className='dual-column' style={{paddingTop:'20px'}}>
         <div className='q-card'>
           <p className='dark'>Where do I park?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>Parking can be around the park or in the water park parking lot across the street from the venue.</p>
         </div>
 
         <div className='q-card'>
           <p className='dark'>Can I get a digital save-the-date?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>Yes! If you go to our Engagement Photos link, you can access the save-the-date there!</p>
         </div>
       </div>
@@ -40,13 +40,13 @@ function FAQ() {
       <div className='dual-column' style={{paddingTop:'20px'}}>
         <div className='q-card'>
           <p className='dark'>Can I spread the news of your wedding/May I bring a friend?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>While we would love to have everyone we’ve known gather to celebrate this wonderful event, we do have limited seating and food. Please do not share the website with those not invited (it has sensitive information) or location and time with just anyone. If we’d want someone to view this page, we will share it with them! :)</p>
         </div>
 
         <div className='q-card'>
           <p className='dark'>What should I wear?</p>
-          <p>-</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
           <p style={{padding:'2%'}}>We ask attendees to dress in church clothes for the ceremony and pictures, but once all the pictures (including a group picture with everyone) are taken we will want to play games for the reception! A men's and women's bathroom are at the venue if you'd like to change into more comfortable clothes.</p>
         </div>
       </div>
@@ -54,16 +54,16 @@ function FAQ() {
       <div className='dual-column' style={{paddingTop:'20px', paddingBottom:'20px'}}>
         <div className='q-card'>
           <p className='dark'>What games will be held?</p>
-          <p>-</p>
-          <p style={{padding:'2%'}}>Games available to play will be cornhole, horseshoe, volleyball, oversized checkers, and spikeball! There will also be a basketball court, sidewalk track, and play areas for littles at the park! If you bring your own pole, you can fish in the lake. We will have two tables set up for checkers and chess as well.
-          We would like to go outside and spend time together with our newly joined family, but if that doesn't permit we will stay inside and bring games in to play. No matter the weather, we will try having a cornhole tournament around 5 PM with a prize and trophy for the winning team!</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
+          <p style={{padding:'2%'}}>Games available to play will be cornhole, horseshoe, volleyball, oversized checkers, and spikeball! There will also be a basketball court, sidewalk track, and play areas for littles at the park! If you bring your own pole, you can fish in the lake. We will have two tables set up for checkers and chess as well.</p>
+          <p style={{padding:'2%'}}>We would like to go outside and spend time together with our newly joined family, but if that doesn't permit we will stay inside and bring games in to play. No matter the weather, we will try having a cornhole tournament around 5 PM with a prize and trophy for the winning team!</p>
         </div>
 
         <div className='q-card'>
           <p className='dark'>When do I arrive?</p>
-          <p>-</p>
-          <p style={{padding:'2%'}}>We ask that the Wedding Party arrive by 10:30 AM to rehearse once more. 
-          Please may friends and family arrive by 1:30 PM (at the latest) to be seated before the ceremony. We would like you to sign in and grab bubbles on the way to your seat. There is only one available entryway so soon after the wedding party will begin lining up for the ceremony. Since we will have a packed day, we would like to begin on time.</p>
+          <p style={{lineHeight:'10pt'}}>-</p>
+          <p style={{padding:'2%'}}>We ask that the Wedding Party arrive by 10:30 AM to rehearse once more.</p>
+          <p style={{padding:'2%'}}>Please may friends and family arrive by 1:30 PM (at the latest) to be seated before the ceremony. We would like you to sign in and grab bubbles on the way to your seat. There is only one available entryway so soon after the wedding party will begin lining up for the ceremony. Since we will have a packed day, we would like to begin on time.</p>
         </div>
         </div>
       </div>
--- src/pages/Venue.tsx
+++ src/pages/Venue.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../styles/App.css';
+import Flower from '../images/bwflower.png'
 
 function Venue() {
   return (
-    <div id='venue'>
+    <div className='venue-background' id='venue'>
+      <div>
+        <img src={Flower} alt="flower" 
+        style={{width:'40%', top:'-30%', right:'-20%', position:'absolute', zIndex:'-1'}}/>
+        <img src={Flower} alt="flower" 
+        style={{width:'40%', top:'-30%', left:'-20%', transform:'rotate(-90deg)', position:'absolute', zIndex:'-1'}}/>
+      </div>
+
       <div className='dual-column'>
-        <div>
-          <p>The Wedding will be hosted at:</p>
+        <div className='address-box'>
+          <p className='fancy-title' style={{fontSize:'30pt'}}>The Wedding will be hosted at:</p>
           <br />
           <p>Wintersmith Lodge,</p>
           <p>1621 Wintersmith Dr,</p>
@@ -17,10 +25,12 @@ function Venue() {
           <p>across the street from the venue.</p>
         </div>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.5840549402824!2d-96.65605428443897!3d34.76606928722143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b369526f215d0b%3A0x9e2ae27c53079950!2sWintersmith%20Lodge!5e0!3m2!1sen!2sus!4v1652998666340!5m2!1sen!2sus" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.5840549402824!2d-96.65605428443897!3d34.76606928722143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b369526f215d0b%3A0x9e2ae27c53079950!2sWintersmith%20Lodge!5e0!3m2!1sen!2sus!4v1652998666340!5m2!1sen!2sus" loading="lazy" className='map'></iframe>
       </div>
 
-      <div></div>
+      <div style={{display:'flex', justifyContent:'center', alignContent:'center'}}>
+        <div className='line'></div>
+      </div>
     </div>
   );
 }
